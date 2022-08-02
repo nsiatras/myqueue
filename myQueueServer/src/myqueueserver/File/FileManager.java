84d804283412b2769407997a5a325862f9c35d92
Nikos Siatras <nsiatras@gmail.com>
2022-08-02 11:21:06 +0300

    MIT License text added to files

--- myQueueServer/src/myqueueserver/File/FileManager.java
+++ myQueueServer/src/myqueueserver/File/FileManager.java
@@ -1,3 +1,26 @@
+/**
+ * MIT License
+ *
+ * Copyright (c) 2022 Nikos Siatras
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
 package myqueueserver.File;
 
 import java.io.*;
@@ -15,7 +38,7 @@ public class FileManager
 
     public static void WriteFile(byte[] bytes, String filepath) throws FileNotFoundException, IOException
     {
-        try (FileOutputStream stream = new FileOutputStream(filepath))
+        try ( FileOutputStream stream = new FileOutputStream(filepath))
         {
             stream.write(bytes);
             stream.close();
@@ -27,7 +50,7 @@ public class FileManager
         File file = new File(filepath);
         if (file.exists() && file.canRead())
         {
-            try (InputStream is = new FileInputStream(file))
+            try ( InputStream is = new FileInputStream(file))
             {
                 long length = file.length();
                 byte[] bytes = new byte[(int) length];
